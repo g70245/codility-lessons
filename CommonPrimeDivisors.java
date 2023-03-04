7db8e9cfe3941fa62f1c80160462c16e1bb27b96
Leon Peng <g70245@gmail.com>
2023-03-04 13:23:28 +0800

    Create CommonPrimeDivisors.java

---
+++ CommonPrimeDivisors.java
@@ -0,0 +1,42 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        // Implement your solution here
+        int Z = A.length;
+        int ans = 0;
+        for (int i = 0; i < Z; i++) {
+            int gcd = A[i] >= B[i] ? gcd(A[i], B[i]) : gcd(B[i], A[i]);
+            if (checkIfHavingSamePrimeDivisors(gcd, A[i]) && checkIfHavingSamePrimeDivisors(gcd, B[i])) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private boolean checkIfHavingSamePrimeDivisors(int gcd, int num) {
+        if (gcd == 1) {
+            return false;
+        } 
+
+        if (gcd == num) {
+            return true;
+        }
+        
+        num /= gcd;
+        gcd = gcd >= num ? gcd(gcd, num) : gcd(num, gcd);
+        return checkIfHavingSamePrimeDivisors(gcd, num);
+    }
+
+    private int gcd (int a, int b) {
+        if (a % b == 0) {
+            return b;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
