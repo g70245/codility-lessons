3c0ac2f76fa90e84e4801918d71e9c801a01f066
Leon Peng <g70245@gmail.com>
2023-03-06 05:20:39 +0800

    Update FibFrog50.java

--- FibFrog50.java
+++ FibFrog50.java
@@ -15,40 +15,42 @@ class Solution {
 
     public int solution(int[] A) {
         // Implement your solution here
-        int riverWidth = A.length + 1;
-        Map<Integer, Integer> dp = new HashMap<>();
-        int minJumps = findMinJumps(A, riverWidth, dp);
-        return minJumps == Integer.MAX_VALUE ? -1 : minJumps;
-    }
-
-    private int findMinJumps(int[] A, int currentEndPos, Map<Integer, Integer> dp) {
-        if(dp.containsKey(currentEndPos)) {
-            return dp.get(currentEndPos);
-        }
-
-        if (fibs.contains(currentEndPos)) {
+        int N = A.length;
+        int width = N + 1;
+        if (fibs.contains(width)) {
             return 1;
         }
 
-        int minJumps = Integer.MAX_VALUE;
-        for (int i = currentEndPos - 1; i >= 1; i--) {
-            if(A[i - 1] == 1 && fibs.contains(currentEndPos - i)) {
-                minJumps = Math.min(minJumps, findMinJumps(A, i, dp));
+        Map<Integer, Integer> dp = new HashMap<>();
+        for (int i = 1; i <= width; i++) {
+            if (i == width || A[i - 1] == 1) {
+                if (fibs.contains(i)) {
+                    dp.put(i, 1);
+                    continue;
+                }
+
+                int min = Integer.MAX_VALUE;
+                for (Map.Entry<Integer, Integer> e : dp.entrySet()) {
+                    if (fibs.contains(i - e.getKey())) {
+                        min = Math.min(min, e.getValue());
+                    }
+                }
+
+                if (min != Integer.MAX_VALUE) {
+                    dp.put(i, min + 1);
+                }
             }
         }
-        minJumps = minJumps == Integer.MAX_VALUE ? Integer.MAX_VALUE : minJumps + 1;
-        dp.put(currentEndPos, minJumps);
-        return minJumps;
+        return dp.containsKey(width) ? dp.get(width) : -1;
     }
 
-    private Set<Integer> generateFibs(int num) {
+    private Set<Integer> generateFibs(int limit) {
         List<Integer> fibs = new ArrayList<>();
         fibs.add(0);
         fibs.add(1);
-        while(fibs.get(fibs.size() - 1) < num) {
+        while(fibs.get(fibs.size() - 1) < limit) {
             fibs.add(fibs.get(fibs.size() - 1) + fibs.get(fibs.size() - 2));
         }
         return fibs.stream().collect(Collectors.toSet());
     }
-
 }
